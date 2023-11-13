a2b163952d004f06a515278a5cb74bf56644d690
Cristian Vicas <cristian.vicas@cs.utcluj.ro>
2023-11-13 19:01:54 +0200

    Minimal example of stable diffusion on cpu and gpu.

---
+++ src/local_image_gen.py
@@ -0,0 +1,33 @@
+import torch
+from diffusers import DiffusionPipeline
+
+# Model card: https://huggingface.co/runwayml/stable-diffusion-v1-5
+# More info: https://huggingface.co/blog/stable_diffusion
+
+# This code runs on 6GB GPU.
+# float16 is not available for CPU
+
+prompt = "A red flower in a pot. The flower has bright green leafs. The pot is from glass."
+device = "cuda"     # for FP16, it fits in < 6G ram
+# device = "cpu"    # Uncomment to run on cpu. WARNING! It can take a looong time!!!
+
+if device == "cuda":
+    pipeline = DiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5",
+                                                 revision="fp16", torch_dtype=torch.float16)
+else:
+    pipeline = DiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", use_safetensors=True)
+
+pipeline.to(device)
+generator = torch.Generator(device).manual_seed(32344)
+
+generator_output = pipeline(prompt, guidance_scale=4.5, num_inference_steps=3, generator=generator)
+image = generator_output.images[0]
+image.save("../test/images/local_image_gen1.jpg")
+
+generator_output = pipeline(prompt, guidance_scale=4.5, num_inference_steps=22, generator=generator)
+image = generator_output.images[0]
+image.save("../test/images/local_image_gen2.jpg")
+
+generator_output = pipeline(prompt, guidance_scale=4.5, num_inference_steps=50, generator=generator)
+image = generator_output.images[0]
+image.save("../test/images/local_image_gen3.jpg")
